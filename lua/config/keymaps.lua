5f3e07397f3de0c9518985556c2455a241fbe2ed
Adam Gray <adamgrayscale@gmail.com>
2024-09-17 19:39:26 -0500

    add toggle term and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- toggleterm
+vim.keymap.set('n', '<C-.>', ':ToggleTerm<CR>', { noremap = true, silent = true })
