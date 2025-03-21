ed80f1cac626ab9c4000a37506b0274403859290
Adam Gray <adamgrayscale@gmail.com>
2025-03-21 15:57:46 -0500

    keymap for code actions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,11 @@
 -- nvim
 vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true })
+
+-- lsp
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, { desc = "Code action" })
 
 -- nvim-tree
 vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
