34694baee66401317762186bf9f73b212101ce3a
Adam Gray <adamgrayscale@gmail.com>
2025-03-21 16:32:15 -0500

    map leader f to format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
 vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = "Format code" })
 
 -- nvim-tree
 vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
