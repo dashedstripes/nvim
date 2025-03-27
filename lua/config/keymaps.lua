0540656af937126b1bb595b99164c45a296b8628
Adam Gray <adam.gray@sanity.io>
2025-03-27 10:23:26 -0500

    map escaping terminal to Esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,7 +20,7 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- toggleterm
 vim.keymap.set('n', '<C-.>', ':ToggleTerm<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<C-.>', [[<C-\><C-n>]], { noremap = true, silent = true })
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
 -- avante
 vim.keymap.set('n', '<C-l>', ':AvanteAsk<CR>', { noremap = true, silent = true })
