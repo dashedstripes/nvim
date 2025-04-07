810ab80627948a353f4d7f96a9b5085aea7e958a
Adam Gray <adam.gray@sanity.io>
2025-04-07 10:04:34 -0500

    use con as well as log for snippet name

--- lua/snippets/js.lua
+++ lua/snippets/js.lua
@@ -1,6 +1,7 @@
 local ls = require("luasnip")
 local t = ls.text_node
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
 
 local rfc = ls.snippet("rfc", {
   t("export default function "), i(1), t("() {"),
@@ -12,11 +13,25 @@ local rfc = ls.snippet("rfc", {
   t({ "", "}" }),
 })
 
-local log = ls.snippet("log", {
-  t("console.log("), i(1), t(");"),
-})
+local log_snippet = {
+  t("console.log('"), i(1), t("', "), rep(1), t(");"),
+}
+
+local log = ls.snippet("log", log_snippet)
+local con = ls.snippet("con", log_snippet)
 
 ls.add_snippets("typescriptreact", {
   rfc,
   log,
+  con,
+})
+
+ls.add_snippets("typescript", {
+  log,
+  con,
+})
+
+ls.add_snippets("javascript", {
+  log,
+  con,
 })
