c0f10fec004d162f5c1978f201bc0dd61a3f6fb3
Adam Gray <adam.gray@sanity.io>
2025-04-07 09:57:02 -0500

    add log snippet

--- lua/snippets/react.lua
+++ lua/snippets/react.lua
@@ -4,14 +4,19 @@ local i = ls.insert_node
 
 local rfc = ls.snippet("rfc", {
   t("export default function "), i(1), t("() {"),
-  t({"", "  return ("}),
-  t({"", "    <div>"}),
-  t({"", "      "}), i(2),
-  t({"", "    </div>"}),
-  t({"", "  );"}),
-  t({"", "}"}),
+  t({ "", "  return (" }),
+  t({ "", "    <div>" }),
+  t({ "", "      " }), i(2),
+  t({ "", "    </div>" }),
+  t({ "", "  );" }),
+  t({ "", "}" }),
+})
+
+local log = ls.snippet("log", {
+  t("console.log("), i(1), t(");"),
 })
 
 ls.add_snippets("typescriptreact", {
-  rfc
+  rfc,
+  log,
 })
